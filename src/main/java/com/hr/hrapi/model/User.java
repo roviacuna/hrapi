81491a0d1bf69c92000360a9de007b1bb999fa1c
roviacuna <roviacuna@gmail.com>
2022-05-24 21:14:55 -0500

    application.properties cofiguration and creating model user

---
+++ src/main/java/com/hr/hrapi/model/User.java
@@ -0,0 +1,57 @@
+package com.hr.hrapi.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastname;
+    private String phone;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/test/java/com/hr/hrapi/HrapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.hrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
