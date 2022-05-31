85099c22e7142d228b29d4decc299992b509c02e
roviacuna <roviacuna@gmail.com>
2022-05-31 10:41:28 -0500

    adding controller class to User

---
+++ src/main/java/com/hr/hrapi/contoller/UserRestController.java
@@ -0,0 +1,24 @@
+package com.hr.hrapi.contoller;
+
+import com.hr.hrapi.response.UserReponseRest;
+import com.hr.hrapi.response.UserResponse;
+import com.hr.hrapi.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserRestController {
+
+    @Autowired
+    public IUserService userService;
+
+    @GetMapping("/users")
+    public UserReponseRest userReponseRest(){
+        UserReponseRest userReponseRest = userService.findUsers();
+        return userReponseRest;
+    }
+
+}
