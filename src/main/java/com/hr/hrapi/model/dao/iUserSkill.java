9a068497b6019dbcb38b32c556ab0a6c7dec80e8
roviacuna <roviacuna@gmail.com>
2022-06-02 20:00:37 -0500

    Creating interface DAO to UserSkill

---
+++ src/main/java/com/hr/hrapi/model/dao/iUserSkill.java
@@ -0,0 +1,7 @@
+package com.hr.hrapi.model.dao;
+
+import com.hr.hrapi.model.UserSkill;
+import org.springframework.data.repository.CrudRepository;
+
+public interface iUserSkill extends CrudRepository<UserSkill, Long> {
+}
